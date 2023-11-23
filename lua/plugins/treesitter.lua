9e6112578c3bbd7c6ea41f903b4089c24ee6770d
Alan Palhau <alanvieirapalhau@hotmail.com>
2023-11-22 23:48:15 -0300

    My Lazy Vim Config (devaslife)

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "cmake",
+        "cpp",
+        "css",
+        "fish",
+        "gitignore",
+        "go",
+        "graphql",
+        "http",
+        "java",
+        "php",
+        "rust",
+        "scss",
+        "sql",
+        "svelte",
+      },
+    },
+  },
+}
